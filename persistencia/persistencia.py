dd6e2a760aab822909caee5531013fa72835131b
davinson vanegas <davidsonvanegas27@gmail.com>
2024-11-02 15:18:24 -0500

    Se modifica el nombre de la conexion en el metodo de crear

--- persistencia/persistencia.py
+++ persistencia/persistencia.py
@@ -5,12 +5,12 @@ from typing import List, Union
 def createUser(query:str, data:List[Union[str,int,float]]):
 
     try: 
-        conexion = BD.CNX()
-        cursor = conexion.cursor()
+        connet = BD.CNX()
+        cursor = connet.cursor()
         cursor.execute(query,data)
-        conexion.commit()
+        connet.commit()
         cursor.close()
-        conexion.close()
+        connet.close()
         print("User created successfully")
     except Exception as e:
         print("I'm really sorry, we are not create your account")
