b1490d9952bf9dbaae975fd0dc6f282fadad2e9d
davinson vanegas <davidsonvanegas27@gmail.com>
2024-11-02 10:06:47 -0500

    Se añade un caturador de error por si la conexion falla por algun motivo

--- persistencia/persistencia.py
+++ persistencia/persistencia.py
@@ -1,13 +1,18 @@
-# import bd.bd as BD
+import bd.bd as BD
+from typing import List, Union
 
-# cursor = BD.CNX.cursor()
-# sentecia = ("INSERT INTO `usuarios` (`id`, `name`, `age`, `cash`, `address`, `phone`, `email`, `location`, `ahorros`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);")
-# DATA = [idUser, name, age, cash, adress, phone, email, savings]
-# try:
-#     cursor.execute(sentecia,DATA)
-#     BD.CNX.commit()
-#     cursor.close()
-#     BD.CNX.close()
-#     print("User created successfully")
-# except ValueError:
-#     print("I'm really sorry, we are not create your account")
+
+def createUser(query:str, data:List[Union[str,int,float]]):
+
+    try: 
+        conexion = BD.CNX()
+        cursor = conexion.cursor()
+        cursor.execute(query,data)
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+        print("User created successfully")
+    except Exception as e:
+        print("I'm really sorry, we are not create your account")
+        print(e)
+
